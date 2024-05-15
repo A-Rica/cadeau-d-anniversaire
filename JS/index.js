2244a92963ad537765cc356dffebb984d1c51fcf
A-Rica <91880772+A-Rica@users.noreply.github.com>
2024-05-15 17:59:57 +0200

    Add files via upload

---
+++ JS/index.js
@@ -0,0 +1,120 @@
+// const ButtonMessage = document.querySelectorAll('.button-message-perso')
+const MessagePerso = document.querySelectorAll('.message-perso')
+// MessagePerso.style.marginTop = "0px";
+console.table(MessagePerso)
+MessagePerso[0].style.marginTop ="0px";
+MessagePerso[1].style.marginTop ="0px";
+MessagePerso[2].style.marginTop ="0px";
+MessagePerso[3].style.marginTop ="0px";
+MessagePerso[4].style.marginTop ="0px";
+MessagePerso[5].style.marginTop ="0px";
+MessagePerso[6].style.marginTop ="0px";
+MessagePerso[7].style.marginTop ="0px";
+MessagePerso[8].style.marginTop ="0px";
+MessagePerso[9].style.marginTop ="0px";
+
+function ClicCoulissantA(){
+
+   if(MessagePerso[0].style.marginTop === "0px"){
+      MessagePerso[0].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[0].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+
+
+function ClicCoulissantB(){
+
+
+   if(MessagePerso[1].style.marginTop === "0px"){
+      MessagePerso[1].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[1].style.marginTop = "-0px"
+   }
+
+   
+} 
+
+function ClicCoulissantC(){
+
+
+   if(MessagePerso[2].style.marginTop === "0px"){
+      MessagePerso[2].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[2].style.marginTop = "-0px"
+   }
+
+   
+} 
+
+function ClicCoulissantD(){
+
+   if(MessagePerso[3].style.marginTop === "0px"){
+      MessagePerso[3].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[3].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+function ClicCoulissantE(){
+
+   if(MessagePerso[4].style.marginTop === "0px"){
+      MessagePerso[4].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[4].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+function ClicCoulissantF(){
+
+   if(MessagePerso[5].style.marginTop === "0px"){
+      MessagePerso[5].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[5].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+function ClicCoulissantI(){
+
+   if(MessagePerso[6].style.marginTop === "0px"){
+      MessagePerso[6].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[6].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+function ClicCoulissantJ(){
+
+   if(MessagePerso[7].style.marginTop === "0px"){
+      MessagePerso[7].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[7].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+function ClicCoulissantK(){
+
+   if(MessagePerso[8].style.marginTop === "0px"){
+      MessagePerso[8].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[8].style.marginTop = "-0px"
+   }
+ 
+} ;
+
+function ClicCoulissantL(){
+
+   if(MessagePerso[9].style.marginTop === "0px"){
+      MessagePerso[9].style.marginTop ="-438.3px";
+   }else{
+      MessagePerso[9].style.marginTop = "-0px"
+   }
+ 
+} ;
